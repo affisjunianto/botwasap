0dc37464fc3f18c47f873a73fe89489c3ab1f02d
affisjunianto <69682437+affisjunianto@users.noreply.github.com>
2020-12-31 01:03:59 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,17 @@
-apt update
-apt upgrade
-apt install
-pkg update && pkg upgrade
+pkg update
+pkg upgrade
+pkg install python
+pkg install python2
+pkg install mc
+pkg install tesseract
+pkg install nodejs
 pkg install wget
 pkg install ffmpeg
-pkg install nodejs
-pkg install tesseract
+clear
+npm i -g ytdl
 npm i -g cwebp
-npm i -g ytdl 
-npm i
+npm i node-tesseract-ocr
 npm i got
+npm i
+clear
+echo "[*]udah selesai nih. Tinggal ketik node index.js"
